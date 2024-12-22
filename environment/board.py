6f453a83dbb3b47cd6f68e6e90e4dff06aad8e76
Kyle Gilbert <144296191+kylegilber@users.noreply.github.com>
2024-12-22 01:29:19 -0500

    Update square.py
    Add in-line documentation and rename class variables to remove redundance in the naming convention.

--- environment/board.py
+++ environment/board.py
@@ -7,17 +7,24 @@ class Board:
 
     def createBoard(self, colors):
         i = 0
-        for row in range(9):
-            for col in range(9):
-                self.squares[row][col] = Square(colors[i], 0)
+        for col in range(9):
+            for row in range(9):
+                self.squares[col][row] = Square(colors[i], 0)
                 i += 1
 
-    def getBoard(self):
-        return [[self.squares[x][y].squareValue for x in range(9)] for y in range(9)]
+    def getValueBoard(self):
+        return [[self.squares[x][y].value for x in range(9)] for y in range(9)]
+    
+    '''
+    def getColorBoard(self):
+        
+        for col in range(9):
+            for row in range(9):
+    '''
 
 
     def printBoard(self):
         for row in range(9):
             for col in range(9):
-                print('|', end=str(self.squares[row][col].squareValue))
+                print('|', end=str(self.squares[row][col].value))
             print("|", end='\n')
--- environment/square.py
+++ environment/square.py
@@ -1,7 +1,20 @@
 import webcolors
 
 class Square:
+    """
+    Representation of an individual square on the Queens' game board
+    """
 
-    def __init__(self, color, value):
-        self.squareColor = webcolors.rgb_to_hex((color[0], color[1], color[2]))
-        self.squareValue = value
+    def __init__(self, color, queen):
+        """
+        Initialize new square instance.
+
+        :param color: ndarray of rgb values [r,g,b]
+        :param queen: 1 if square has a queen on it, 0 otherwise 
+        """
+
+        # Convert color from rbg array to hex code string
+        self.color = webcolors.rgb_to_hex((color[0], color[1], color[2]))
+
+        # Track whether or not the square holds a queen
+        self.value = queen
