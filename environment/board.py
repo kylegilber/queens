5b007febaab25a3d6d7059342c965c447be1c0d6
Kyle Gilbert <144296191+kylegilber@users.noreply.github.com>
2024-12-23 17:34:52 -0500

    Update board.py
    Update getSquareValues() to return a tuple so the values can be added to a set. Create a less-than method for class Board to compare two Board objects -- helpful for priority queue. Add heuristic() function to help implement the less-than method.

--- environment/board.py
+++ environment/board.py
@@ -2,6 +2,7 @@ from environment.square import Square
 
 from collections import defaultdict
 
+
 class Board:
     """
     Representation of the Queens' game board where each square in the 9x9 grid is an object of class Square.
@@ -17,14 +18,25 @@ class Board:
         self.squares = [[Square(colors[y][x], 0) for y in range(9)] for x in range(9)]
     
 
+    def __lt__(self, other):
+        """
+        Compare Board objects based on their heuristic value
+
+        :param other: a Board class object
+        :returns: whether the first Board object's heuristic value is less than the second
+        """
+
+        return self.heuristic() < other.heuristic()
+
+
     def getSquareValues(self):
         """
         Representation of the game board where each element in the 9x9 grid is a value indicating if the square has a Queen on it.
 
         :returns: a matrix of square values
         """
-
-        return [[self.squares[x][y].value for x in range(9)] for y in range(9)]
+        
+        return tuple(tuple(self.squares[x][y].value for x in range(9)) for y in range(9))
     
 
     def groupByColor(self):
@@ -41,6 +53,22 @@ class Board:
                 colors[square.color].append(square.value)
 
         return colors
+
+
+    def heuristic(self):
+        """
+        Compute the 'distance' a Board state is from the goal state
+
+        :returns: the amount of rows, columns, and color blocks that do not have exactly one queen
+        """
+
+        values = self.getSquareValues()
+        colors = self.groupByColor()
+        incorrectRows = sum(1 for col in range(9) if values[col].count(1) != 1)
+        incorrectCols = sum(1 for row in zip(*values) if row.count(1) != 1)
+        incorrectClrs = sum(1 for row in list(colors.keys()) if colors[row].count(1) != 1)
+
+        return incorrectRows + incorrectCols + incorrectClrs
 
 
     def printBoard(self):
