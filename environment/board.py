fb7ab405b08d15d799539d2b5e991ba8022c8e26
Kyle Gilbert <144296191+kylegilber@users.noreply.github.com>
2024-12-22 23:57:16 -0500

    Update board.py
    Add in-line documentation and create the groupByColor() function.

--- environment/board.py
+++ environment/board.py
@@ -1,29 +1,44 @@
 from environment.square import Square
 
+from collections import defaultdict
+
 class Board:
+    """
+    Representation of the Queens' gameboard where each square in the 9x9 grid is an object of class Square.
+    """
 
-    def __init__(self):
-        self.squares = [[0 for x in range(9)] for y in range(9)]
+    def __init__(self, colors):
+        """
+        Initialize a new 9x9 board instance with no placed queens.
 
-    def createBoard(self, colors):
-        i = 0
-        for col in range(9):
-            for row in range(9):
-                self.squares[col][row] = Square(colors[i], 0)
-                i += 1
+        :param colors: matrix with the hex color code for each square
+        """
 
-    def getValueBoard(self):
-        return [[self.squares[x][y].value for x in range(9)] for y in range(9)]
+        self.squares = [[Square(colors[y][x], 0) for y in range(9)] for x in range(9)]
+        self.values = [[self.squares[x][y].value for x in range(9)] for y in range(9)]
     
-    '''
-    def getColorBoard(self):
-        
+    
+    def groupByColor(self):
+        """
+        Group the squares on the board by color.
+
+        Returns a dictionary whose key is a hex color code, and value is a list with the value of each square of that color.
+        """
+
+        colors = defaultdict(list)
         for col in range(9):
             for row in range(9):
-    '''
+                square = self.squares[col][row]
+                colors[square.color].append(square.value)
+
+        return colors
 
 
     def printBoard(self):
+        """
+        Output the current value of the board's squares
+        """
+
         for row in range(9):
             for col in range(9):
                 print('|', end=str(self.squares[row][col].value))
